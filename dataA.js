0daea9004ca8349604bb3873994f3fe4e5afe7af
cao059921-tech <cao059921@gmail.com>
2026-04-26 15:02:44 +0900

    Add files via upload

---
+++ dataA.js
@@ -0,0 +1,41 @@
+const units = [
+/*1*/
+ [
+  { kanji: "尨眉皓髪", frontYomi: "ぼうび", backYomi: "こうはつ", hide: "front" },
+  { kanji: "破戒無塹", frontYomi: "はかい", backYomi: "むざん", hide: "back" },
+  { kanji: "月中蟾蜍", frontYomi: "げっちゅう(の)", backYomi: "せんじょ", hide: "back" },
+  { kanji: "三跪九叩", frontYomi: "さんき", backYomi: "きゅうこう", hide: "front" },
+  { kanji: "撥乱反正", frontYomi: "はつらん", backYomi: "はんせい", hide: "front" },
+  { kanji: "鳳友鸞交", frontYomi: "ほうゆう", backYomi: "らんこう", hide: "back" },
+  { kanji: "三董三沐", frontYomi: "さんくん", backYomi: "さんもく", hide: "back" },
+  { kanji: "影駭驚震", frontYomi: "えいがい", backYomi: "きょうしん", hide: "front" },
+  { kanji: "狗吠緇衣", frontYomi: "くはい", backYomi: "しい", hide: "back" },
+  { kanji: "虛融澹泊", frontYomi: "こゆう", backYomi: "たんぱく", hide: "back" }
+],
+/*2*/
+[
+  { kanji: "兔起鶻落", frontYomi: "とき", backYomi: "こつらく", hide: "back" },
+  { kanji: "鴟目虎吻", frontYomi: "しもく", backYomi: "こふん", hide: "front" },
+  { kanji: "五行相剋", frontYomi: "ごぎょう", backYomi: "そうこく", hide: "back" },
+  { kanji: "鸞翔鳳集", frontYomi: "らんしょう", backYomi: "ほうしゅう", hide: "front" },
+  { kanji: "霓裳羽衣", frontYomi: "げいしょう", backYomi: "うい", hide: "front" },
+  { kanji: "自己韜晦", frontYomi: "じこ", backYomi: "とうかい", hide: "back" },
+  { kanji: "豪放磊落", frontYomi: "ごうほう", backYomi: "らいらく", hide: "back" },
+  { kanji: "画脂鏤氷", frontYomi: "がし", backYomi: "ろうひょう", hide: "back" },
+  { kanji: "旱天慈雨", frontYomi: "かんてん", backYomi: "じう", hide: "front" },
+  { kanji: "筆削褒貶", frontYomi: "ひっさく", backYomi: "ほうへん", hide: "back" }
+],
+/*3*/
+[
+  { kanji: "発揚蹈厲", frontYomi: "はつよう", backYomi: "とうれい", hide: "back" },
+  { kanji: "逍遥法外", frontYomi: "しょうよう", backYomi: "ほうがい", hide: "front" },
+  { kanji: "俛首帖耳", frontYomi: "ふしゅ", backYomi: "ちょうじ", hide: "back" },
+  { kanji: "風岸孤峭", frontYomi: "ふうがん", backYomi: "こしょう", hide: "back" },
+  { kanji: "先従隗始", frontYomi: "せんじゅう", backYomi: "かいし", hide: "back" },
+  { kanji: "厥角稽首", frontYomi: "けっかく", backYomi: "けいしゅ", hide: "front" },
+  { kanji: "桃弧棘矢", frontYomi: "とうこ", backYomi: "きょくし", hide: "front" },
+  { kanji: "三釁三浴", frontYomi: "さんきん", backYomi: "さんよく", hide: "front" },
+  { kanji: "牝牡驪黄", frontYomi: "ひんぼ", backYomi: "りこう", hide: "back" },
+  { kanji: "北轅適楚", frontYomi: "ほくえん", backYomi: "てきそ", hide: "front" }
+]
+];
