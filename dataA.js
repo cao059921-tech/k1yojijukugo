c89bbd0d803f45cb03230a9adaefc99dab2cbba6
cao059921-tech <cao059921@gmail.com>
2026-04-26 14:31:58 +0900

    Add files via upload

---
+++ dataA.js
@@ -0,0 +1,109 @@
+const units = [
+
+  /*
+  ============================
+  四字熟語問題1
+  ============================
+  */
+  [
+    {
+      kanji: "魑魅魍魎",
+      frontYomi: "ちみ",
+      backYomi: "もうりょう",
+      hide: "back"
+    },
+    {
+      kanji: "紆余曲折",
+      frontYomi: "うよ",
+      backYomi: "きょくせつ",
+      hide: "front"
+    },
+    {
+      kanji: "波瀾万丈",
+      frontYomi: "はらん",
+      backYomi: "ばんじょう",
+      hide: "front"
+    },
+    {
+      kanji: "遏悪揚善",
+      frontYomi: "あつあく",
+      backYomi: "ようぜん",
+      hide: "front"
+    },
+    {
+      kanji: "勁草春暉",
+      frontYomi: "けいそう",
+      backYomi: "しゅんき",
+      hide: "front"
+    },
+    {
+      kanji: "在邇求遠",
+      frontYomi: "ざいじ",
+      backYomi: "きゅうえん",
+      hide: "front"
+    },
+    {
+      kanji: "足音跫然",
+      frontYomi: "そくおん",
+      backYomi: "きょうぜん",
+      hide: "back"
+    },
+    {
+      kanji: "影駭驚震",
+      frontYomi: "えいがい",
+      backYomi: "きょうしん",
+      hide: "front"
+    },
+    {
+      kanji: "朱脣皓歯",
+      frontYomi: "しゅしん",
+      backYomi: "こうし",
+      hide: "front"
+    },
+    {
+      kanji: "黜陟幽明",
+      frontYomi: "ちゅっちょく",
+      backYomi: "ゆうめい",
+      hide: "front"
+    }
+  ],
+
+  /*
+  ============================
+  四字熟語問題2
+  ============================
+  */
+  [
+    {
+      kanji: "温故知新",
+      frontYomi: "おんこ",
+      backYomi: "ちしん",
+      hide: "front"
+    },
+    {
+      kanji: "異口同音",
+      frontYomi: "いく",
+      backYomi: "どうおん",
+      hide: "front"
+    },
+    {
+      kanji: "一期一会",
+      frontYomi: "いちご",
+      backYomi: "いちえ",
+      hide: "front"
+    }
+  ],
+// ユニット4（テスト用）
+[
+  { kanji: "四面楚歌", yomi: "しめん", hide: "front" },
+  { kanji: "電光石火", yomi: "でんこう", hide: "front" },
+  { kanji: "一石二鳥", yomi: "いっせき", hide: "front" },
+  { kanji: "異口同音", yomi: "いく", hide: "front" },
+  { kanji: "温故知新", yomi: "おんこ", hide: "front" },
+  { kanji: "危機一髪", yomi: "きき", hide: "front" },
+  { kanji: "十人十色", yomi: "じゅうにん", hide: "front" },
+  { kanji: "自業自得", yomi: "じごう", hide: "front" },
+  { kanji: "以心伝心", yomi: "いしん", hide: "front" },
+  { kanji: "晴耕雨読", yomi: "せいこう", hide: "front" }
+]
+];
